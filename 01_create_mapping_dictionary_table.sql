757e23ad26025bfa63b6c398029fc0e6641ecb1a
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-10-12 16:19:39 +0300

    Update 01_create_mapping_dictionary_table.sql

--- 01_create_mapping_dictionary_table.sql
+++ 01_create_mapping_dictionary_table.sql
@@ -3,3 +3,6 @@ CREATE TABLE power_bi_cc_field_mapping(
 	attribute varchar(100),
 	field varchar(100)
 );
+
+/*Add index to dictionary to faster view*/
+CREATE INDEX IXX ON power_bi_cc_field_mapping(attribute);
